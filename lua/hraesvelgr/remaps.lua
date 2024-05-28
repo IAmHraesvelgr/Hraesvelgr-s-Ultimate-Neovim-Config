682f22a6e1f6acf7fd5790a61765bac145c89545
IAmHraesvelgr <alexis.kahne@free.fr>
2024-05-28 13:27:39 +0200

    hraesvelgr's ultimate neovim config

--- lua/hraesvelgr/remaps.lua
+++ lua/hraesvelgr/remaps.lua
@@ -9,3 +9,12 @@ vim.keymap.set('n', '<leader>ee', vim.cmd.NvimTreeFocus)
 
 -- ZenMode
 vim.keymap.set('n', '<leader>zm', vim.cmd.ZenMode)
+
+-- NvTerm
+vim.keymap.set('n', '<A-h>', function() require('nvterm.terminal').toggle('horizontal') end)
+vim.keymap.set('n', '<A-v>', function() require('nvterm.terminal').toggle('vertical') end)
+vim.keymap.set('n', '<A-i>', function() require('nvterm.terminal').toggle('float') end)
+
+vim.keymap.set('t', '<A-h>', function() require('nvterm.terminal').toggle('horizontal') end)
+vim.keymap.set('t', '<A-v>', function() require('nvterm.terminal').toggle('vertical') end)
+vim.keymap.set('t', '<A-i>', function() require('nvterm.terminal').toggle('float') end)
