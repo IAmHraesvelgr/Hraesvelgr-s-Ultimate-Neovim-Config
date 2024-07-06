8ed1d5a43c57f2bcfbff661a493c239ed6b08281
IAmHraesvelgr <alexis.kahne@free.fr>
2024-07-06 19:22:36 +0200

    hraesvelgr's ultimate neovim config

---
+++ lua/plugins/treesitter-context.lua
@@ -0,0 +1,11 @@
+return {
+    {
+        "nvim-treesitter/nvim-treesitter-context",
+        event = "VeryLazy",
+        config = function ()
+            require('treesitter-context').setup({
+                enable = true
+            })
+        end
+    }
+}
