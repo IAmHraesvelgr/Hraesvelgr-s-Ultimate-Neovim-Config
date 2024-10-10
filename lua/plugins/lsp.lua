99251c1e776b5a4074222b1cc41b8b7ee1a411fc
IAmHraesvelgr <alexis.kahne@free.fr>
2024-10-10 15:05:14 +0200

    hraesvelgr's ultimate neovim config

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,12 +1,28 @@
 return {
     {
-        { 'williamboman/mason.nvim' },
+        { 
+            'williamboman/mason.nvim',
+            opts = {
+                ensure_installed = {
+                    "html-lsp",
+                    "css-lsp",
+                    "typescript-language-server",
+                    "prettier",
+                    "pyright",
+                    "black",
+                    "clangd",
+                    "cmake-language-server",
+                    "csharp-language-server",
+                    "lua-language-server"
+                }
+            }
+        },
         { 'williamboman/mason-lspconfig.nvim' },
         { 'VonHeikemen/lsp-zero.nvim',        branch = 'v3.x' },
         { 'neovim/nvim-lspconfig' },
         { 'hrsh7th/cmp-nvim-lsp' },
         { 'hrsh7th/nvim-cmp' },
         { 'L3MON4D3/LuaSnip' },
-        event = "VeryLazy",
+        event = "VeryLazy"
     }
 }
