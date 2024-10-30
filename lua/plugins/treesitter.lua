cf8a141d6427e575aade925ef7ac59e3acd7bfca
IAmHraesvelgr <alexis.kahne@free.fr>
2024-10-30 13:18:38 +0100

    hraesvelgr's ultimate neovim config

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -15,13 +15,6 @@ return {
                 },
                 indent = { enable = true },
             })
-
-            require("nvim-treesitter.configs").setup({
-                highlight = {
-                    enable = true,
-                    additional_vim_regex_highlighting = false,
-                },
-            })
         end,
     },
 }
