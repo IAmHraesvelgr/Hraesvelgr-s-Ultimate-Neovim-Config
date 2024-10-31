b19e0593468cfdc151bcdc37e247f22bd84c5598
Hræsvelgr <alexis.kahne@free.fr>
2024-10-31 15:18:11 +0100

    hraesvelgr's ultimate neovim config

--- lua/hraesvelgr/configs/nvchad.lua
+++ lua/hraesvelgr/configs/nvchad.lua
@@ -2,48 +2,48 @@ local map = vim.keymap.set
 
 -- Tabufline
 map("n", "<Tab>", function()
-	require("nvchad.tabufline").next()
+  require("nvchad.tabufline").next()
 end)
 
 map("n", "<S-Tab>", function()
-	require("nvchad.tabufline").prev()
+  require("nvchad.tabufline").prev()
 end)
 
 map("n", "<leader>bc", function()
-	require("nvchad.tabufline").close_buffer()
+  require("nvchad.tabufline").close_buffer()
 end)
 
 map("n", "<leader>br", function()
-	require("nvchad.tabufline").move_buf(1)
+  require("nvchad.tabufline").move_buf(1)
 end)
 
 map("n", "<leader>bl", function()
-	require("nvchad.tabufline").move_buf(-1)
+  require("nvchad.tabufline").move_buf(-1)
 end)
 
 -- Terminal
 map({ "n", "t" }, "<A-i>", function()
-	require("nvchad.term").toggle({ pos = "float", id = "floatTerm" })
+  require("nvchad.term").toggle({ pos = "float", id = "floatTerm" })
 end)
 
 map({ "n", "t" }, "<A-h>", function()
-	require("nvchad.term").toggle({ pos = "bo sp", id = "spTerm" })
+  require("nvchad.term").toggle({ pos = "bo sp", id = "spTerm" })
 end)
 
 map({ "n", "t" }, "<A-v>", function()
-	require("nvchad.term").toggle({ pos = "bo vsp", id = "vspTerm" })
+  require("nvchad.term").toggle({ pos = "bo vsp", id = "vspTerm" })
 end)
 
 -- Themes
 map("n", "<leader>ts", function()
-	require("base46").toggle_transparency()
+  require("base46").toggle_transparency()
 end)
 
 map("n", "<leader>th", function()
-	require("nvchad.themes").open()
+  require("nvchad.themes").open()
 end)
 
 -- Rename
 map("n", "<leader>rr", function()
-	require("nvchad.lsp.renamer")()
+  require("nvchad.lsp.renamer")()
 end)
