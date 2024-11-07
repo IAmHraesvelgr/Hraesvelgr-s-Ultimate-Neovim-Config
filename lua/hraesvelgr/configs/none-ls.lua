7c349f473ae06743ed2d3449b69db3dd81f16bdf
Hræsvelgr <alexis.kahne@free.fr>
2024-11-07 14:35:03 +0100

    hraesvelgr's ultimate neovim config

--- lua/hraesvelgr/configs/none-ls.lua
+++ lua/hraesvelgr/configs/none-ls.lua
@@ -2,34 +2,35 @@ local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 local null_ls = require("null-ls")
 
 null_ls.setup({
-  sources = {
-    null_ls.builtins.formatting.black,
-    null_ls.builtins.formatting.isort,
-    null_ls.builtins.formatting.prettier,
-    null_ls.builtins.formatting.stylua,
-    null_ls.builtins.formatting.gofumpt,
-    null_ls.builtins.formatting.clang_format.with({
-      extra_args = {
-        "-style={BasedOnStyle: Microsoft, PointerAlignment: Left, IndentWidth: 2}",
-      },
-    }),
-  },
-  on_attach = function(client, bufnr)
-    if client.supports_method("textDocument/formatting") then
-      vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-      vim.api.nvim_create_autocmd("BufWritePre", {
-        group = augroup,
-        buffer = bufnr,
-        callback = function()
-          vim.lsp.buf.format({
-            async = false,
-            bufnr = bufnr,
-            filter = function()
-              return client.name == "null-ls"
-            end,
-          })
-        end,
-      })
-    end
-  end,
+	sources = {
+		null_ls.builtins.formatting.black,
+		null_ls.builtins.formatting.isort,
+		null_ls.builtins.formatting.prettier,
+		null_ls.builtins.formatting.stylua,
+		null_ls.builtins.formatting.gofumpt,
+		null_ls.builtins.formatting.clang_format.with({
+			extra_args = {
+				"-style={BasedOnStyle: Microsoft, PointerAlignment: Left, IndentWidth: 2}",
+			},
+		}),
+	},
+	on_attach = function(client, bufnr)
+		if client.supports_method("textDocument/formatting") then
+			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				group = augroup,
+				buffer = bufnr,
+				callback = function()
+					vim.lsp.buf.format({
+						async = false,
+						bufnr = bufnr,
+						filter = function()
+							return client.name == "null-ls"
+						end,
+					})
+				end,
+			})
+		end
+	end,
+	timeout_ms = 2500,
 })
