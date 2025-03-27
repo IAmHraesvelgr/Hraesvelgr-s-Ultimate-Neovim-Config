d2d551cf2d9ef7629914d11fc6cec435ac850da1
Hræsvelgr <alexis.kahne@free.fr>
2025-03-27 14:04:50 +0100

    hraesvelgr's ultimate neovim config

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -6,20 +6,4 @@ return {
 			require("hraesvelgr.configs.none-ls")
 		end,
 	},
-	{
-		"jay-babu/mason-null-ls.nvim",
-		event = { "BufReadPre", "BufNewFile" },
-		config = function()
-			require("mason-null-ls").setup({
-				ensure_installed = {
-					"black",
-					"clang-format",
-					"gofumpt",
-					"isort",
-					"prettier",
-					"stylua",
-				},
-			})
-		end,
-	},
 }
