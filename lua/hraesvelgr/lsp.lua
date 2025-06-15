bc7ea40fe3de747ad355701972aa12071e4b3971
IAmHraesvelgr <alexis.kahne@free.fr>
2025-06-15 23:45:56 +0200

    hraesvelgr's ultimate neovim config

--- lua/hraesvelgr/lsp.lua
+++ lua/hraesvelgr/lsp.lua
@@ -7,59 +7,52 @@ local defaults = configs.defaults
 local lspconfig = require("lspconfig")
 
 local servers = {
-	"bashls",
-	"clangd",
-	"cmake",
-	"omnisharp",
-	"cssls",
-	"gopls",
-	"html",
-	"lua_ls",
-	"pyright",
-	"ts_ls",
-	"rust_analyzer",
-	"jdtls",
+  "bashls",
+  "clangd",
+  "cmake",
+  "omnisharp",
+  "cssls",
+  "gopls",
+  "html",
+  "lua_ls",
+  "pyright",
+  "ts_ls",
+  "rust_analyzer",
+  "jdtls",
 }
 
 for _, lsp in ipairs(servers) do
-	-- Temp fix for Omnisharp
-	if lsp == "omnisharp" then
-		lspconfig[lsp].setup({
-			on_init = on_init,
-			on_attach = function(client)
-				client.server_capabilities.documentFormattingProvider = false
-				client.server_capabilities.documentRangeFormattingProvider = false
-				on_attach()
-			end,
-			capabilities = capabilities,
-			defaults = defaults,
-			cmd = {
-				"omnisharp",
-				"-z",
-				"DotNet:enablePackageRestore=false",
-				"--encoding",
-				"utf-8",
-				"--languageserver",
-				"Sdk:IncludePrereleases=true",
-			},
-		})
-	elseif lsp == "clangd" then
-		lspconfig[lsp].setup({
-			on_init = on_init,
-			on_attach = function(client)
-				client.server_capabilities.documentFormattingProvider = false
-				client.server_capabilities.documentRangeFormattingProvider = false
-				on_attach()
-			end,
-			capabilities = capabilities,
-			defaults = defaults,
-		})
-	else
-		lspconfig[lsp].setup({
-			on_init = on_init,
-			on_attach = on_attach(),
-			capabilities = capabilities,
-			defaults = defaults,
-		})
-	end
+  -- Temp fix for Omnisharp
+  if lsp == "omnisharp" then
+    lspconfig[lsp].setup({
+      on_init = on_init,
+      on_attach = function(client)
+        client.server_capabilities.documentFormattingProvider = false
+        client.server_capabilities.documentRangeFormattingProvider = false
+        on_attach()
+      end,
+      capabilities = capabilities,
+      defaults = defaults,
+      cmd = {
+        "omnisharp",
+        "-z",
+        "DotNet:enablePackageRestore=false",
+        "--encoding",
+        "utf-8",
+        "--languageserver",
+        "Sdk:IncludePrereleases=true",
+      },
+    })
+  else
+    lspconfig[lsp].setup({
+      on_init = on_init,
+      on_attach = function(client)
+        client.server_capabilities.documentFormattingProvider = false
+        client.server_capabilities.documentRangeFormattingProvider = false
+        on_attach()
+      end,
+      capabilities = capabilities,
+      defaults = defaults,
+    })
+  end
 end
