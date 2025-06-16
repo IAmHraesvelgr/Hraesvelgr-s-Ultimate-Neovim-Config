a7691968404cdcb54ea26a8444efe184421a771d
IAmHraesvelgr <alexis.kahne@free.fr>
2025-06-16 14:43:10 +0200

    hraesvelgr's ultimate neovim config

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,36 +1,34 @@
 return {
-{
-		"saghen/blink.cmp",
-		version = "1.*",
-		event = { "InsertEnter", "CmdLineEnter" },
-		opts = {
-			snippets = { preset = "luasnip" },
-			cmdline = { enabled = true },
-			appearance = { nerd_font_variant = "normal" },
-			fuzzy = {
-				prebuilt_binaries = {
-					download = false,
-				},
-				implementation = "lua",
+	"saghen/blink.cmp",
+	version = "1.*",
+	event = { "InsertEnter", "CmdLineEnter" },
+	opts = {
+		snippets = { preset = "luasnip" },
+		cmdline = { enabled = true },
+		appearance = { nerd_font_variant = "normal" },
+		fuzzy = {
+			prebuilt_binaries = {
+				download = false,
 			},
-			sources = { default = { "lsp", "snippets", "buffer", "path" } },
+			implementation = "lua",
+		},
+		sources = { default = { "lsp", "snippets", "buffer", "path" } },
 
-			keymap = {
-				preset = "default",
-				["<CR>"] = { "accept", "fallback" },
-				["<C-b>"] = { "scroll_documentation_up", "fallback" },
-				["<C-f>"] = { "scroll_documentation_down", "fallback" },
-				["<Tab>"] = { "select_next", "snippet_forward", "fallback" },
-				["<S-Tab>"] = { "select_prev", "snippet_backward", "fallback" },
-			},
+		keymap = {
+			preset = "default",
+			["<CR>"] = { "accept", "fallback" },
+			["<C-b>"] = { "scroll_documentation_up", "fallback" },
+			["<C-f>"] = { "scroll_documentation_down", "fallback" },
+			["<Tab>"] = { "select_next", "snippet_forward", "fallback" },
+			["<S-Tab>"] = { "select_prev", "snippet_backward", "fallback" },
+		},
 
-			completion = {
-				ghost_text = { enabled = true },
-				documentation = {
-					auto_show = true,
-					auto_show_delay_ms = 200,
-					window = { border = "single" },
-				},
+		completion = {
+			ghost_text = { enabled = true },
+			documentation = {
+				auto_show = true,
+				auto_show_delay_ms = 200,
+				window = { border = "single" },
 			},
 		},
 	},
