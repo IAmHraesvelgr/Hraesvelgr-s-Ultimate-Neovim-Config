025c6dca6785265d6c5163609729f1d04cdb3654
IAmHraesvelgr <alexis.kahne@free.fr>
2025-06-17 00:26:11 +0200

    hraesvelgr's ultimate neovim config

--- lua/hraesvelgr/settings.lua
+++ lua/hraesvelgr/settings.lua
@@ -10,9 +10,9 @@ o.cursorline = true
 o.cursorlineopt = "number"
 
 o.expandtab = true
-o.shiftwidth = 4
+o.shiftwidth = 2
 o.smartindent = true
-o.tabstop = 4
+o.tabstop = 2
 o.linebreak = true
 
 opt.fillchars = { eob = " " }
